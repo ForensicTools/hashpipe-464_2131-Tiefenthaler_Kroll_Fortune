ecb365915a7770921f4df7b26d048cb8ac73a405
emt1891 <l33tforlif3@Mariott-Front-Desk.local>
2013-11-02 15:25:00 -0400

    Initial HashPipe.pl File
    Added basic pseudo-code as well as basic formatting.

---
+++ HashPipe.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+#function to capture images of hdd's
+#use dd for this, make sure to exclude the removable media
+
+
+
+#function to copy memory
+#use dd for this
+
+
+
+#function to copy logs over
+#probably use something like cp or tail all of the logs over, undecided
+
+
+
+#function to collect current system data
+#includes ifconfig, netstat, top, ps
+
+
+
+
+#GUI Section, mostly number based
+
+while (
+
+print options
+
+Ask user for input
+Chomp input, check for correct formatting
+
+create if and else statements to determine what functions to run, functions listed above
+make this in a while loop that only ends with a ctrl + c or a quit option
+
+)
