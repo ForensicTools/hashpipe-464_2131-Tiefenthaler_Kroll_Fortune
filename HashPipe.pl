7405f1d3548d9c2850d3f719538c277861ceaa5b
emt1891 <l33tforlif3@Mariott-Front-Desk.local>
2013-11-02 16:15:23 -0400

    Added Project Folder functionality
    Worked with Adam to add functionality to create folder to put project
    images and other files into

--- HashPipe.pl
+++ HashPipe.pl
@@ -3,6 +3,24 @@ use strict;
 use warnings;
 
 
+#open files here; need files for current sys stats?
+#grab time, create new directory to put current working files into
+
+
+#asks user for input to what new folder is going to be called
+print "Please enter the name of this project: ";
+my $folder = <STDIN>;
+print $folder;
+
+#gets current working directory, and makes new folder for the project
+my $directory = `pwd`;
+chomp $directory;
+my $newDir = "mkdir $directory/$folder";
+system($newDir);
+
+#absolute path to the folder that was just created
+my $workingDir = "$directory/$folder";
+
 #function to capture images of hdd's
 #use dd for this, make sure to exclude the removable media
 
@@ -14,13 +32,13 @@ use warnings;
 
 
 #function to copy logs over
-#probably use something like cp or tail all of the logs over, undecided
+#probably use something like cp or tail all of the logs over to removable media?
 
 
 
 #function to collect current system data
 #includes ifconfig, netstat, top, ps
-
+#either single bash script or run system or `` commands
 
 
 
